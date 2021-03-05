43f976aeb388d3d4cdddeaadad3dfbbbe812cdd9
huyhnueit68 <huyhiephoaa2@gmail.com>
2021-03-05 10:43:01 +0700

    hihi

--- Thoi Tiet/index.php
+++ Thoi Tiet/index.php
@@ -1,21 +1,5 @@
 <?php
-$apiKey = "412610a74c218e9e763db58706e65210";
-$cityId = "1564676";
-$googleApiUrl = "http://api.openweathermap.org/data/2.5/weather?id=" . $cityId . "&lang=en&units=metric&APPID=" . $apiKey;
-
-$ch = curl_init();
-
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_URL, $googleApiUrl);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch, CURLOPT_VERBOSE, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-$response = curl_exec($ch);
-
-curl_close($ch);
-$data = json_decode($response);
-$currentTime = time();
+    
 ?>
 
 <!doctype html>
